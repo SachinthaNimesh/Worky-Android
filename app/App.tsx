c3146d3e71382c2b79b22c2932ddbdf4e090f5ca
Sachintha Dissanayake <124299595+SachinthaNimesh@users.noreply.github.com>
2025-04-07 11:54:36 +0530

    Update App.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -5,7 +5,7 @@ import { WebView } from 'react-native-webview';
 const App: React.FC = () => {
   return (
     <WebView 
-      source={{ uri: 'https://rubber-farmer-assistant.netlify.app' }} 
+      source={{ uri: 'https://6f96b5fa-5913-4129-9d39-4259881e4655.e1-us-east-azure.choreoapps.dev' }} 
       style={styles.container}
       startInLoadingState={true}
       renderLoading={() => (
