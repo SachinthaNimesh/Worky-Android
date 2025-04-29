6ec1aac6c3764d7653d002ed7e15919c76ead6a9
SachinthaNimesh <sanisenevirathna@gmail.com>
2025-04-29 11:04:06 +0530

    location api done

--- app/App.tsx
+++ app/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import { View, StyleSheet, ActivityIndicator } from 'react-native';
 import { WebView } from 'react-native-webview';
+import Location from './Location';
 
 const App: React.FC = () => {
   return (
-    <WebView 
-      source={{ uri: 'https://6f96b5fa-5913-4129-9d39-4259881e4655.e1-us-east-azure.choreoapps.dev' }} 
-      style={styles.container}
-      startInLoadingState={true}
-      renderLoading={() => (
-        <View style={styles.loaderContainer}>
-          <ActivityIndicator size="large" color="#0000ff" />
-        </View>
-      )}
-    />
+    <View style={styles.container}>
+      <WebView 
+        source={{ uri: 'https://87abc270-1269-4d98-8dad-e53781a1ae52.e1-us-east-azure.choreoapps.dev' }} 
+        style={styles.webview}
+        startInLoadingState={true}
+        renderLoading={() => (
+          <View style={styles.loaderContainer}>
+            <ActivityIndicator size="large" color="#0000ff" />
+          </View>
+        )}
+      />
+      <Location />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  webview: {
     flex: 1,
   },
   loaderContainer: {
